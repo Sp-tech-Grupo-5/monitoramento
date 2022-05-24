ff9c9a816aabf99b90b5e389610d0291a09d7561
raylanef <88952014+raylanef@users.noreply.github.com>
2022-05-24 13:26:48 -0300

    Update Connection.java

--- src/main/java/connection/Connection.java
+++ src/main/java/connection/Connection.java
@@ -14,13 +14,11 @@ public class Connection {
     public Connection(){
         dataSource= new  BasicDataSource();
         dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-<<<<<<< HEAD
-        //com.mysql.cj.jdbc.Driver
+
         dataSource.setUrl("jdbc:sqlserver://projeto-rxmonitoramento.database.windows.net:1433;database=bd-grupo5-rxmonitoramento;user=admin"
                 + "-rxmonitoramento@projeto-rxmonitoramento;password={your_password_here};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*"
                 + ".database.windows.net;loginTimeout=30;");
-        //"jdbc:mysql://localhost:3306/rxmonitoramento"
-=======
+        
         dataSource.setUrl("jdbc:sqlserver://projeto-rxmonitoramento.database.windows.net:1433;"
                 + "database=bd-grupo5-rxmonitoramento;"
                 + "user=admin-rxmonitoramento@projeto-rxmonitoramento;"
@@ -28,11 +26,11 @@ public class Connection {
                 + "encrypt=true;"
                 + "trustServerCertificate=false;"
                 + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
->>>>>>> dac39a3b5d695f6dcbc8604d1f168b348dff14f8
         dataSource.setUsername("admin-rxmonitoramento");
         dataSource.setPassword("2ads@grupo5");
         
     }
+    
       public Connection(Boolean mysql){
         dataSource= new  BasicDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
